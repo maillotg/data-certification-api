dc4bc564cc0d58a0f7499af4d0df85c004b205b0
gaetan maillot <maillotgaetan@gmail.com>
2021-06-30 17:35:11 +0200

    Load model ...

--- api/app.py
+++ api/app.py
@@ -1,8 +1,20 @@
 
 from fastapi import FastAPI
+import joblib
+
+
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.linear_model import LinearRegression
 
 app = FastAPI()
 
+model = joblib.load('./model.joblib', mmap_mode=None)
 
 # define a root `/` endpoint
 @app.get("/")
@@ -10,7 +22,10 @@ def index():
     return {"ok": True}
 
 @app.get("/predict")
-def predict():
+def predict(acousticness, danceability, duration_ms, energy, explicit ,id , instrumentalness, key, liveness, loudness, mode,name, release_date, speechiness, tempo, valence, artist):
+
+    ### TODO
+
     prediction = {'artist':None, 'song_name':None, 'popularity':None}
     return prediction
 
